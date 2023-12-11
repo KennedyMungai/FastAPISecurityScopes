2c190799384f2e9748933bcc9bac3e2fcafbebdc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 11:32:29 +0300

    Added some code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 """The main app for the application"""
-from fastapi import FastAPI
+from datetime import datetime, timedelta
+from typing import Annotated
 
+from fastapi import Depends, FastAPI, HTTPException, Security, status
+from fastapi.security import (OAuth2PasswordBearer, OAuth2PasswordRequestForm,
+                              SecurityScopes)
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel, ValidationError
 
 app = FastAPI(
     title="Security Scopes",
@@ -8,6 +15,59 @@ app = FastAPI(
     version="0.1.0"
 )
 
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Chains",
+        "email": "alicechains@example.com",
+        "hashed_password": "$2b$12$gSvqqUPvlXP2tfVFaWK1Be7DlH.PKZbv5H8KnzzVgXXbVxpva.pFm",
+        "disabled": True,
+    },
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+    
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+    
+    
+class UserInDB(User):
+    hashed_password: str
+    
+    
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="token",
+    scopes={"me": "Read information about the current user.", "items": "Read items."}
+)
+
+
+def hash_password(_password):
+    return pwd_context.hash(_password)
+
+def verify_password(_plain_password, _hashed_password):
+    return pwd_context.verify(_plain_password, _hashed_password)
 
 @app.get("/")
 async def root() -> dict[str, str]:
