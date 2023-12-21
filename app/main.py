cb2b4c90d09c9c84f0475fd503dcac4c6cbbfac9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-21 14:18:37 +0300

    Made some simple changes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-"""The main app for the application"""
+"""Security Scopes Example"""
 from datetime import datetime, timedelta
 from typing import Annotated
 
@@ -9,11 +9,6 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel, ValidationError
 
-app = FastAPI(
-    title="Security Scopes",
-    description="A simple security scopes experiment",
-    version="0.1.0"
-)
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -43,37 +38,4 @@ class Token(BaseModel):
     token_type: str
     
     
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-    
-    
-class UserInDB(User):
-    hashed_password: str
-    
-    
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="token",
-    scopes={"me": "Read information about the current user.", "items": "Read items."}
-)
-
-
-def hash_password(_password):
-    return pwd_context.hash(_password)
-
-def verify_password(_plain_password, _hashed_password):
-    return pwd_context.verify(_plain_password, _hashed_password)
-
-@app.get("/")
-async def root() -> dict[str, str]:
-    """The root endpoint of the application
-
-    Returns:
-        dict[str, str]: A simple message
-    """
-    return {"message": "Hello World"}
+class TokenData(BaseModel)
